a5480100c30453ce717af22eeee1d7bcb1527b48
Guida <margarida.s.martins00@gmail.com>
2020-12-07 10:34:25 +0000

    Initial commit Web Server

---
+++ Web_Server/src/main/java/ies/g25/aLIVE/ALiveApplication.java
@@ -0,0 +1,13 @@
+package ies.g25.aLIVE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ALiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ALiveApplication.class, args);
+	}
+
+}
---
+++ Web_Server/src/test/java/ies/g25/aLIVE/ALiveApplicationTests.java
@@ -0,0 +1,13 @@
+package ies.g25.aLIVE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ALiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
