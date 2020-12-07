c0b08fa3efa27485e48e5f757869cdd4bb61e760
bastos-01 <p.bastos112@gmail.com>
2020-12-07 18:10:49 +0000

    basic blood pressure generator

--- sensing/generator.py
+++ sensing/generator.py
@@ -11,8 +11,19 @@ class Generator:
         sigma = 10.0
 
         while True:
-            data = data = np.random.randn(1) * sigma + mu
+            data = np.random.randn(1) * sigma + mu
             print(data)
+
+    def gen_blood_pressure(self):
+        systolic_mu = 130
+        diastolic_mu = 85
+        sigma = 20.0
+
+        while True:
+            systolic = np.random.randn(1) * sigma + systolic_mu
+            diastolic = np.random.randn(1) * sigma + diastolic_mu
+            if ((systolic - diastolic) > 30) and (systolic > 90) and (diastolic > 50):
+                print(systolic , " ----- " , diastolic)
 
 
 if __name__ == "__main__":
@@ -23,4 +34,5 @@ if __name__ == "__main__":
 
     g = Generator(args.hb)
 
-    g.gen_heart_beats(g.hearbeat)
+    #g.gen_heart_beats(g.hearbeat)
+    #g.gen_blood_pressure()
