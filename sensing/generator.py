adee9bc55318267304b0ca1317e7e742d8cfd758
bastos-01 <p.bastos112@gmail.com>
2020-12-07 18:46:20 +0000

    basic temperature generator

--- sensing/generator.py
+++ sensing/generator.py
@@ -11,8 +11,8 @@ class Generator:
         sigma = 10.0
 
         while True:
-            data = np.random.randn(1) * sigma + mu
-            print(data)
+            hb = np.random.randn(1) * sigma + mu
+            print(hb)
 
     def gen_blood_pressure(self):
         systolic_mu = 130
@@ -24,6 +24,15 @@ class Generator:
             diastolic = np.random.randn(1) * sigma + diastolic_mu
             if ((systolic - diastolic) > 30) and (systolic > 90) and (diastolic > 50):
                 print(systolic , " ----- " , diastolic)
+    
+    def gen_body_temp(self):
+        mu = 37
+        sigma = 1.0
+
+        while True:
+            temperature = np.random.randn(1) * sigma + mu
+            if(34 < temperature < 42):
+                print(temperature)
 
 
 if __name__ == "__main__":
@@ -36,3 +45,4 @@ if __name__ == "__main__":
 
     #g.gen_heart_beats(g.hearbeat)
     #g.gen_blood_pressure()
+    #g.gen_body_temp()
