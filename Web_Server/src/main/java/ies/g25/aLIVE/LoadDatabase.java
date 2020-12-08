9cdde0a95f850fa7ea13151a2789843f4fe04914
Rui-FMF <ruifmf2000@gmail.com>
2020-12-08 17:26:25 +0000

    fixes and post

--- Web_Server/src/main/java/ies/g25/aLIVE/LoadDatabase.java
+++ Web_Server/src/main/java/ies/g25/aLIVE/LoadDatabase.java
@@ -10,11 +10,11 @@ class LoadDatabase {
 
     return args -> {
       log.info("Preloading " + Prep.save(new Patient("pass1", "manel123", "manel@jmail.com", "Manel Ribeiro", 
-      62, new Date(2019, 10, 11), "good")));
+      62, new Date(), "good")));
       log.info("Preloading " + Prep.save(new Patient("pass2", "silvia69", "silv@dmail.com", "Silvia Matos", 
-      74, new Date(2019, 2, 5), "very good")));
+      74, new Date(), "very good")));
       log.info("Preloading " + Prep.save(new Patient("pass3", "ze321", "ze@dmail.com", "Zé Matos", 
-      75, new Date(2019, 3, 5), "very good")));
+      75, new Date(), "very good")));
     };
   }
 
---
+++ Web_Server/src/main/java/ies/g25/aLIVE/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package ies.g25.aLIVE.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ies.g25.aLIVE.model.User;
+
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
