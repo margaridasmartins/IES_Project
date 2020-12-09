f697821eb9c86c1c75c6886f8d9d87ae7a2443fb
Guida <margarida.s.martins00@gmail.com>
2020-12-09 14:56:03 +0000

    body temp repository and entity´

---
+++ Web_Server/src/main/java/ies/g25/aLIVE/model/BodyTemperature.java
@@ -0,0 +1,60 @@
+package ies.g25.aLIVE.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name="BOODY_TEMP")
+public class BodyTemperature{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    // date of the mesure
+    @Column(name="date",nullable = false, updatable = false)
+    @CreationTimestamp
+    private Date date;
+
+    // Blood Temperature values
+    @Column(name = "bodytemp")
+    private double bodyTemp;
+
+    //Patient Id
+    @ManyToOne()
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    public BodyTemperature(){}
+
+    public BodyTemperature(double bodytemp){
+        this.bodyTemp=bodytemp;
+    }
+
+    public void setbodyTemp(double bodytemp){
+        this.bodyTemp=bodytemp;
+    }
+    public double getbodyTemp(){
+        return this.bodyTemp;
+    }
+
+    public Date getDate(){
+        return this.date;
+    }
+    public Patient getPatient(){
+        return this.patient;
+    }
+    public void setPatient(Patient p){
+        this.patient=p;
+    }
+}
---
+++ Web_Server/src/main/java/ies/g25/aLIVE/repository/BodyTemperatureRepository.java
@@ -0,0 +1,13 @@
+package ies.g25.aLIVE.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import ies.g25.aLIVE.model.*;
+
+@Repository
+public interface BodyTemperatureRepository extends JpaRepository<BodyTemperature, Long>{
+
+    List<BodyTemperature> findByPatient(Patient p);
+}
