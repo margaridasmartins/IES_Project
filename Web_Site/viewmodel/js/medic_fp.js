ac6c088fde064b3b79ab40804ffca6287d23f7f7
Francisca Barros <kikas4323@gmail.com>
2020-12-13 12:34:17 +0000

    even more responsive :D

--- Web_Site/viewmodel/js/medic_fp.js
+++ Web_Site/viewmodel/js/medic_fp.js
@@ -19,38 +19,54 @@ $(document).ready(function () {
             allPatientsArray.push(value);
         } 
     });
+    console.log(allPatientsArray);
 
     allPatientsArray.forEach(p => {
         console.log(p);
-        $("#patientSection").append(`<div class="row" style="margin-top: 3%;">
+        var today = new Date().toLocaleDateString(); 
+        if (new Date(p['last_check']).toLocaleDateString() < today ){
+            $("#patientSection").append(`<div class="row" style="margin-top: 3%;">
                                     <div class="col-md-12">
-                                    <a href="utente_info.html" >
-                                        <div class="card " style="background-color: lightgreen;" >
-                                            <div class="card-body">
-                                                <div class="row ">
-                                                <div class="col-md-2 my-auto">
-                                                    <img src="./images/old_man.jpeg" style="max-width:100px; max-height:100px;">
-                                                </div>
-                                                <div class="col-md-4 my-auto"> 
-                                                    <b>Name: </b> 
-                                                    <em id="patientName">${p['full_name']} </em>
-                                                </div>
-                                                <div class="col-md-2 my-auto">
-                                                    <b>Age: </b>
-                                                    <em id="patientAge">${p['age']} </em>
-                                                </div>
-                                                <div class="col-md-4 my-auto">
-                                                    <p><b>Last Check: </b> None</p>
-                                                </div>
+                                        <a href="#" id="currentPatient" value="${p['id']}" onHover="myFunction();">
+                                            <div class="card " style="background-color: lightgreen;" >
+                                                <div class="card-body">
+                                                    <div class="row ">
+                                                        <div class="col-md-2 my-auto">
+                                                            <img src="./images/old_man.jpeg" style="max-width:100px; max-height:100px;">
+                                                        </div>
+                                                        <div class="col-md-4 my-auto"> 
+                                                            <b>Name: </b> 
+                                                            <em>${p['full_name']} </em>
+                                                        </div>
+                                                        <div class="col-md-2 my-auto">
+                                                            <b>Age: </b>
+                                                            <em>${p['age']} </em>
+                                                        </div>
+                                                        <div class="col-md-4 my-auto">
+                                                            <b>Last Check: </b>
+                                                            <em>${new Date(p['last_check']).toLocaleDateString()} </em>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </a>
+                                        </a>
                                     </div>
                                 </div>`);
+        }
     });
-
-    console.log(allPatientsArray);
+    
+    
+    $("#currentPatient").click(function() {
+        //console.log("#currentPatient");
+        var id = document.getElementById("currentPatient").getAttribute("value");
+        for(var i in allPatientsArray){
+            if (allPatientsArray[i]['id'] == id){
+                localStorage.setItem('currentPatient', JSON.stringify(allPatientsArray[i]));
+            }
+        }
+        console.log(localStorage.getItem('currentPatient'))
+        document.getElementById("currentPatient").setAttribute('href', 'utente_info.html');
+    });
     
 
     /* $.each(allPatientsArray, function(index, value) {
