d265ba36787716652c8e18f4a8bf410cb612bd8f
Rui-FMF <ruifmf2000@gmail.com>
2020-12-15 08:58:10 +0000

    patient atributes

--- Web_Server/src/main/java/ies/g25/aLIVE/model/Patient.java
+++ Web_Server/src/main/java/ies/g25/aLIVE/model/Patient.java
@@ -32,6 +32,20 @@ public class Patient extends User {
     @Column(name = "current_state")
     private String current_state;
 
+    @Column(name = "med_conditions")
+    private ArrayList<String> med_conditions;
+
+    @Column(name = "medication")
+    private ArrayList<String> medication; //dic medicamento:quantidade?
+
+    @Column(name = "weight")
+    private double weight;
+
+    @Column(name = "height")
+    private double height;
+
+
+
     //Assigned professional
     @ManyToOne
     @JoinColumn(name = "professional_id")
